a5c32b8367fe5cd4cb4c502fc7c28584ef616be4
phinney <20641413+fzyxh@users.noreply.github.com>
2022-03-02 20:53:05 +0800

    config

--- src/config.js
+++ src/config.js
@@ -11,7 +11,22 @@ var config = {
 	auth:{
 		activeKey : "activeKey",
 		checkActive: false
-	}
+	},
+    mysql_connection_info:{
+        host     : 'localhost',      
+        user     : 'root',             
+        password : 'root',      
+        port: '3306',                  
+        database: 'ques4u'
+    },
+    table_names:{
+        users_info : 'users_info',
+        groups_info : 'groups_info',
+        questionnaires_info : 'questionnaires_info',
+        questions_info : 'questions_info',
+        tags_info : 'tags_info',
+        questionnaires_results : 'questionnaires_results'
+    }
 };
 
-module.exports = config;
+module.exports = config, mysql_connection;
