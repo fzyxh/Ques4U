dbba3ef91dec1f1316321b5838d06bb748ff33f8
phinney <20641413+fzyxh@users.noreply.github.com>
2022-03-04 10:39:57 +0800

    add some explain

--- src/services/register_group.js
+++ src/services/register_group.js
@@ -5,15 +5,19 @@ const Connection = require('mysql/lib/Connection');
 const res = require('express/lib/response');
 var app = express();
 
+// creat the connection to mysql server
 var connection = mysql.createConnection(config.mysql_connection_info);
 connection.connect();
 
+// module.exports : export the function as a class
 module.exports = function(group_id,group_name,master_id,group_rulers,group_members) {
+    // store the information when creat a new object
     this.Group_id = group_id;
     this.Group_name = group_name;
     this.Master_id = master_id;
     this.Group_rulers = group_rulers;
     this.Group_members = group_members;
+    // method of register
     this.register = function() {
         let info = {
             group_id : this.Group_id,
